a42f84e41b3aef35a9bef5744bd33f811b96f6f8
Daniel T. Santos <danieltex@gmail.com>
2021-06-10 19:00:40 -0300

    Using ResponseStatusException instead of ControllerAdvice for exception response messages. It was mapping all unhandled exceptions to INTERNAL_SERVER_ERROR

--- src/main/kotlin/com/github/danieltex/faerunapp/exceptions/SelfOperationException.kt
+++ src/main/kotlin/com/github/danieltex/faerunapp/exceptions/SelfOperationException.kt
@@ -2,6 +2,8 @@ package com.github.danieltex.faerunapp.exceptions
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.server.ResponseStatusException
 
 @ResponseStatus(HttpStatus.BAD_REQUEST, reason = "Cannot make operation to itself")
-class SelfOperationException : RuntimeException("Cannot make operation to itself")
+class SelfOperationException : ResponseStatusException(HttpStatus.BAD_REQUEST,
+    "Cannot make operation to itself")
--- src/main/kotlin/com/github/danieltex/faerunapp/exceptions/WaterPocketNotFoundException.kt
+++ src/main/kotlin/com/github/danieltex/faerunapp/exceptions/WaterPocketNotFoundException.kt
@@ -2,6 +2,10 @@ package com.github.danieltex.faerunapp.exceptions
 
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.server.ResponseStatusException
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-class WaterPocketNotFoundException(id: Int) : RuntimeException("Water Pocket '$id' not found")
+class WaterPocketNotFoundException(id: Int) : ResponseStatusException(
+    HttpStatus.NOT_FOUND,
+    "Water Pocket '$id' not found"
+)
