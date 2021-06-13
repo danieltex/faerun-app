9f8415de4291632ad62cfda5592bde66b33e77bf
Daniel T. Santos <danieltex@gmail.com>
2021-06-13 11:55:29 -0300

    Adding OpenAPI documentation

--- src/main/kotlin/com/github/danieltex/faerunapp/controllers/WaterPocketController.kt
+++ src/main/kotlin/com/github/danieltex/faerunapp/controllers/WaterPocketController.kt
@@ -13,6 +13,11 @@ import com.github.danieltex.faerunapp.dtos.toDTO
 import com.github.danieltex.faerunapp.dtos.toEntity
 import com.github.danieltex.faerunapp.entities.WaterPocketEntity
 import com.github.danieltex.faerunapp.services.WaterPocketService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
@@ -35,27 +40,54 @@ class WaterPocketController(
     private val waterPocketService: WaterPocketService
 ) {
 
+    @Operation(summary="Cadastra um bolsão e retorna as informações resultantes do cadastro")
+    @ApiResponse(responseCode = "201", description = "Bolsão criado")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun createWaterPocket(@Valid @RequestBody waterPocket: NewWaterPocketDTO): WaterPocketDTO {
-    fun createWaterPocket(@RequestBody waterPocket: WaterPocketDTO): WaterPocketDTO {
         val created = waterPocketService.save(waterPocket.toEntity())
         return created.toDTO()
     }
 
+    @Operation(summary="Lista todos os bolsões cadastrados.")
+    @ApiResponse(responseCode = "200", description = "Retorna uma lista com todos os bolsões")
     @GetMapping(consumes = [MediaType.ALL_VALUE])
     fun getWaterPocketBatch(): WaterPocketBatchDTO {
         val waterPockets = waterPocketService.findAll().map(WaterPocketEntity::toDTO)
         return WaterPocketBatchDTO(waterPockets)
     }
 
+    @Operation(summary = "Informa os dados do bolsão solicitado.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Bolsão encontrado"),
+            ApiResponse(responseCode = "404", description = "Bolsão não encontrado", content = [Content()])
+        ]
+    )
     @GetMapping("/{id}", consumes = [MediaType.ALL_VALUE])
-    fun getWaterPocket(@PathVariable("id") id: Int): WaterPocketDTO {
+    fun getWaterPocket(
+        @Parameter(description = "ID do bolsão solicitado")
+        @PathVariable("id") id: Int
+    ): WaterPocketDTO {
         return waterPocketService.findById(id).toDTO()
     }
 
+    @Operation(summary = "O bolsão especificado no path pega um empréstimo com bolsão especificado no corpo da requisição")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Empréstimo concedido"),
+            ApiResponse(
+                responseCode = "400",
+                description = "O bolsão solicitado não tem capacidade suficiente para conceder o empréstimo",
+                content = [Content()]
+            ),
+            ApiResponse(responseCode = "404", description = "Um dos bolões não foi encontrado", content = [Content()])
+        ]
+    )
     @PostMapping("/{id}/borrow")
     fun borrow(
+        @Parameter(description = "ID do bolsão solicitante")
+        @Valid
         @PathVariable("id") debtorId: Int,
         @RequestBody loanRequest: LoanRequestDTO
     ): WaterPocketDTO {
@@ -66,8 +98,23 @@ class WaterPocketController(
         ).toDTO()
     }
 
+    @Operation(summary = "O bolsão devedor paga uma quantia para o bolsão pagador especificado no corpo da requisição.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Pagamento realizado"),
+            ApiResponse(
+                responseCode = "400",
+                description = "O bolsão devedor não tem capacidade suficiente para realizar o pagamento ou a quantia " +
+                    "especificada é maior que o valor do empréstimo contraído",
+                content = [Content()]
+            ),
+            ApiResponse(responseCode = "404", description = "Um dos bolões não foi encontrado", content = [Content()])
+        ]
+    )
     @PostMapping("/{id}/settle")
     fun settle(
+        @Parameter(description = "ID do bolsão solicitante")
+        @Valid
         @PathVariable("id") debtorId: Int,
         @RequestBody paymentRequest: PaymentRequestDTO
     ): WaterPocketDTO {
@@ -78,24 +125,48 @@ class WaterPocketController(
         ).toDTO()
     }
 
+    @Operation(summary = "Informa o status de dívida do bolsão solicitado.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Lista de dívidas do bolsão"),
+            ApiResponse(responseCode = "404", description = "Bolsão não encontrado", content = [Content()])
+        ]
+    )
     @GetMapping("/{id}/debt", consumes = [MediaType.ALL_VALUE])
-    fun debt(@PathVariable("id") id: Int): DebitListDTO {
+    fun debt(
+        @Parameter(description = "ID do bolsão solicitado")
+        @PathVariable("id") id: Int
+    ): DebitListDTO {
         return waterPocketService.findAllDebts(id).toDTO()
     }
 
+    @Operation(summary = "Retorna um relatório de quais transferências são necessárias para quitar todos os empréstimos realizados no planeta.")
+    @ApiResponse(responseCode = "200", description = "Lista otimizada de todas as operações")
     @GetMapping("/balance", consumes = [MediaType.ALL_VALUE])
     fun balance(): BalanceDTO {
         return waterPocketService.getOptimizedBalance()
     }
 
+    @Operation(summary = "Realiza o pagamento de todos os empréstimos da melhor forma possível")
+    @ApiResponse(responseCode = "200", description = "Lista otimizada de todas as operações")
     @PutMapping("/settle-all", consumes = [MediaType.ALL_VALUE])
     fun settleAll(): SettleOperationsDTO {
         return waterPocketService.settleAll()
     }
 
+
+    @Operation(summary = "Retorna uma lista com todas as operações de um bolsão desde a sua criação, mais recentes primeiro.")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Operações do bolsão"),
+            ApiResponse(responseCode = "404", description = "Bolsão não encontrado", content = [Content()])
+        ]
+    )
     @GetMapping("/{id}/events", consumes = [MediaType.ALL_VALUE])
-    fun events(@PathVariable("id") id: Int): List<EventDTO> {
+    fun events(
+        @Parameter(description = "ID do bolsão solicitado")
+        @PathVariable("id") id: Int
+    ): List<EventDTO> {
         return waterPocketService.findEvents(id)
     }
 }
-
